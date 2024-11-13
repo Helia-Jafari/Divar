eb2e3cd5657c62181e6c73634e28e24d0fb426d6
dr.heliajafari@gmail.com <dr.heliajafari@gmail.com>
2024-11-13 16:58:22 +0330

    change 1

--- Divar/Controllers/AddController.cs
+++ Divar/Controllers/AddController.cs
@@ -1,6 +1,5 @@
 ﻿using Divar.Dbs;
 using Microsoft.AspNetCore.Mvc;
-using Practise1Divar.Db;
 
 namespace Practise1Divar.Controllers
 {
@@ -12,18 +11,25 @@ namespace Practise1Divar.Controllers
 
             _db = db;
         }
-        [HttpPost]
-        public IActionResult Index(Advertisement advertisement)
+        public IActionResult Index()
         {
-            if (!ModelState.IsValid) { return View(); }
-            var model = new Advertisement()
-            {
-                BasePrice = advertisement.BasePrice
-            }
-            _db.
-            _db.SaveChanges();
-
-
+            return View();
         }
+        //[HttpPost]
+        //public IActionResult AddAd(Advertisement advertisement)
+        //{
+            //if (!ModelState.IsValid) { return View(); }
+            //var model = new Advertisement()
+            //{
+            //    BasePrice = advertisement.BasePrice
+            //}
+            //_db.Advertisements.Add(new Advertisement
+            //{
+
+            //})
+            //_db.SaveChanges();
+            //return View();
+
+        //}
     }
 }
